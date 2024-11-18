410d6fc81e56199dc655085c3943bc3e8880813f
selinasanovska <selinasanovska@live.se>
2024-11-18 14:45:39 +0100

    first changes

--- dig-till/src/App.js
+++ dig-till/src/App.js
@@ -1,24 +1,49 @@
 import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Navbar, Container, Nav } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container py-4">
+      <Top/>
+      <Bottom/>
     </div>
+  );
+}
+
+function Top() {
+  return (
+      <div>
+          <div className="jumbotron text-center App-header">
+              <h1>Digitalt utbildningsmaterial</h1>
+              <p>Välkommen!</p>
+          </div>
+          <div>
+              <Navbar className="custom-navbar-background" variant="dark">
+                  <Container>
+                      <Navbar.Brand href="#home">Startsida</Navbar.Brand>
+                      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                      <Navbar.Collapse id="basic-navbar-nav">
+                          <Nav className="me-auto">
+                              <Nav.Link href="#notes" disabled>Anteckningar (kommande funktion)</Nav.Link>
+                              <Nav.Link href="#to-do" disabled>To-do (kommande funktion)</Nav.Link>
+                          </Nav>
+                      </Navbar.Collapse>
+                  </Container>
+              </Navbar>
+          </div>
+      </div>
+  )
+}
+
+function Bottom() {
+  return (
+      <Navbar className="custom-navbar-background" bg="dark" variant="dark"> {/*jesper css*/}
+          <Container>
+              <Navbar.Brand className="mx-auto">Ett projekt i Digital Tillgänglighet av Dennis och Selin</Navbar.Brand>
+          </Container>
+      </Navbar>
   );
 }
 
